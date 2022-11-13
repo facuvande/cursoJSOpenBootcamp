21670caac6a68af8f1a02dbe7e01ef741c6fab7f
Facundo Vandecaveye <facundovs84@hotmail.com.ar>
2022-11-13 16:47:14 -0300

    Primer Desafio

---
+++ index.js
@@ -0,0 +1,5 @@
+// Este es el primer mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+// Este va a ser el mensaje q se envie para despedir
+console.log("Esta es la puerta de salida al proyecto")
